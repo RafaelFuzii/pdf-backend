420cfd6c9e063f192006308d5197406aead38f64
rafael Yuzo Fuzii <yuzofuzii@gmail.com>
2026-06-07 16:35:49 -0300

    feature: adicionado logica para quando tiver lotado o uso da IA pegar outro modelo

--- src/google/googleController.ts
+++ src/google/googleController.ts
@@ -2,12 +2,11 @@ import { Request, Response } from "express";
 import { GoogleService } from "./googleService";
 
 export class GoogleController {
-    constructor(private googleService: GoogleService) {}
+    constructor(private googleService: GoogleService) { }
 
     async processarPDF(req: Request, res: Response) {
-        console.log(req.file)
-        await this.googleService.processarPDF(req.file as Express.Multer.File);
-        res.status(200).json({ message: "PDF processado com sucesso" });
+        const resultado = await this.googleService.processarPDF(req.file as Express.Multer.File, req.params.empresaId as string);
+        res.status(200).json({ message: "PDF processado com sucesso", resultado });
     }
 
 }
--- src/google/googleRepository.ts
+++ src/google/googleRepository.ts
@@ -1,8 +1,15 @@
-import bcrypt from "bcryptjs";
 import { Prisma } from "../../generated/prisma/client";
+import { JsonArray } from "../../generated/prisma/internal/prismaNamespace";
 import { prisma } from "../database/prisma";
 
 
 export class GoogleRepository {
-
+    async salvarDRE(empresaId: string, dadosDRE: JsonArray) {
+        return await prisma.dRE.create({
+            data: {
+                empresaId,
+                dados: dadosDRE
+            }
+        });
+    }
 }
